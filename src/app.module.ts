7b960ad33f1eedf624bfb26fa298c36e047bb578
2Bartek2 <ostrowskibartosz85@gmail.com>
2023-01-14 16:21:57 +0100

    Connecting the configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: 'env',
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(TypeOrmConfig),
+  ],
   controllers: [],
   providers: [],
 })
